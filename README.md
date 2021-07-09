5a2148129dfd2334b5a0fd66fe6a0249620c4988
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2021-07-09 21:12:03 +0430

    Add source code

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# auto-merge-oracle-box
+# Auto merger box with ergo node
+
+This code developed for merge reward oracle box with [ergo node](http://github.com/ergoplatform).
+
+## Setup
+### Prerequisite
+  * python:3.+
+### Getting Started
+
+First clone the repository from Github and switch to the new directory:
+```
+$ git clone https://github.com/zargarzadehm/reward-oracle-merger-ergo
+$ cd reward-oracle-merger-ergo
+```
+
+Install project dependencies:
+```
+$ pip3 install -r requirements.txt
+```
+
+Use `--help` or `-h` for see options:
+```
+$ python3 mergeOracle.py --help
+```
+```
+usage: mergeOracle.py [-h] --ip 127.0.0.1:9053 --apiKey hello --oracleAddress
+                      9fzRcctiWfzoJyqGtPWqoXPuxSmFw6zpnjtsQ1B6jSN514XqH4q
+                      [--mergeToAddress 9fzRcctiWfzoJyqGtPWqoXPuxSmFw6zpnjtsQ1B6jSN514XqH4q]
+                      [--fee 0.0011] [--maxValue 0.18] [--maxBox 50]
+                      [--minBox 30] [--check]
+
+Merge oracle box.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --ip, -i 127.0.0.1:9053   Your node ip
+  --apiKey, -k hello    Your node api-key
+  --oracleAddress, -o 9fzRcctiWfzoJyqGtPWqoXPuxSmFw6zpnjtsQ1B6jSN514XqH4q Your oracle address
+  --mergeToAddress, -m 9fzRcctiWfzoJyqGtPWqoXPuxSmFw6zpnjtsQ1B6jSN514XqH4q  your destination address (optional: default is your 'oracleAddress')
+  --fee, -f 0.0011      Fee for send transaction (optional: default is 0.0011)
+  --maxValue, -b 0.18   The box must have a maximum of 'maxValue' erg enough to participate in the transaction (optional: default
+                        is 0.02)
+  --maxBox, -bx 50      The maximum number of boxes that can be present in a transaction (optional: default is 50)
+  --minBox, -bn 30      The minimum number of boxes that can be present in a transaction (optional: default is 30)
+  --check, -c           This parameter help to check information of tx before send it and question for send or no TX (optional: default is False)
+```
+
+For start service with best practice options using this:
+```
+$ python3 auto_merge_oracle_box.py -i YOUR_NODE_ADDRESS -k YOUR_API_KEY -o YOUR_ORACLE_ADDRESS
+```
+Example:
+```
+$ python3 auto_merge_oracle_box.py -i 127.0.0.1:9053 -k hello -o 9fzRcctiWfzoJyqGtPWqoXPuxSmFw6zpnjtsQ1B6jSN514XqH4q
+```
---
+++ mergeOracle.py
@@ -0,0 +1,105 @@
+import requests
+import json
+from datetime import datetime
+import argparse
+
+
+def check(ip, api_key, fee, oracle_address, merge_to_address, max_value, max_box, min_box, check_param):
+	headers = {
+		'accept': 'application/json',
+		'api_key': api_key,
+		'Content-Type': 'application/json'
+	}
+	boxes = list()
+	valreq = 0
+	res = requests.get('http://{}/wallet/boxes/unspent'.format(ip), headers=headers).json()
+	for value in res:
+		if len(boxes) < max_box:
+			if value['address'] == oracle_address and value['box']['value'] <= max_value:
+				valreq += value['box']['value']
+				resBox = requests.get('http://{}/utxo/byIdBinary/{}'.format(ip, str(value['box']['boxId'])), headers=headers).json()
+				boxes.append(resBox["bytes"])
+			else:
+				continue
+		else:
+			break
+
+	valreq = int(valreq - fee)
+	if not valreq > (fee * 2) or not len(boxes) >= min_box:
+		log('There is not enough balance / box according to conditions for merge them, now there are {} ERG and {} box'.format((valreq / 1e9), len(boxes)))
+		exit(0)
+	out = {
+		"requests": [
+			{
+				"address": merge_to_address,
+				"value": valreq
+			}
+		],
+			"fee": fee,
+			"inputsRaw": boxes
+	}
+	log("value of transaction is {} with fee {} number of input is {} to addresss:\n{}".format(valreq / 1e9, fee / 1e9, len(boxes), merge_to_address))
+	
+	if check_param:
+		while True:
+			val_in = input('Is ok send transaction (y/n):\n')
+			log("input is: {}".format(val_in))
+			if str(val_in) == ["y", "Y", "yes", "YES"]:
+				res = requests.post('http://{}/wallet/transaction/send'.format(ip), headers=headers, data=json.dumps(out)).json()
+				log("sent transaction with id:" + res)
+				exit(0)
+			elif str(val_in) == ["n", "N", "no", "NO"]:
+				log("END")
+				exit(0)
+	else:
+		res = requests.post('http://{}/wallet/transaction/send'.format(ip), headers=headers, data=json.dumps(out)).json()
+		log("sent transaction with id:" + res)
+		exit(0)
+
+class CustomHelpFormatter(argparse.HelpFormatter):
+    def _format_action_invocation(self, action):
+        if not action.option_strings or action.nargs == 0:
+            return super()._format_action_invocation(action)
+        default = self._get_default_metavar_for_optional(action)
+        args_string = self._format_args(action, default)
+        return ', '.join(action.option_strings) + ' ' + args_string
+
+fmt = lambda prog: CustomHelpFormatter(prog)
+parser = argparse.ArgumentParser(formatter_class=fmt, description='Merge oracle box.')
+
+parser.add_argument('--ip', '-i', metavar='127.0.0.1:9053', type=str, required=True, help='Your node ip')
+parser.add_argument('--apiKey', '-k', metavar='hello', type=str, required=True, help='Your node api-key')
+parser.add_argument('--oracleAddress', '-o', metavar='9fzRcctiWfzoJyqGtPWqoXPuxSmFw6zpnjtsQ1B6jSN514XqH4q', type=str, required=True, help='Your oracle address')
+parser.add_argument('--mergeToAddress', '-m', metavar='9fzRcctiWfzoJyqGtPWqoXPuxSmFw6zpnjtsQ1B6jSN514XqH4q', type=str, required=False, default='', help='your destination address (optional: default is your \'oracleAddress\')')
+parser.add_argument('--fee', '-f', metavar='0.0011', type=float, default=0.0011, required=False, help='Fee for send transaction (optional: default is %(default)s)')
+parser.add_argument('--maxValue', '-b', metavar='0.18', type=float, default=0.02, required=False, help='The box must have a maximum of \'maxValue\' erg enough to participate in the transaction (optional: default is %(default)s)')
+parser.add_argument('--maxBox', '-bx', metavar='50', type=int, default=50, required=False, help='The maximum number of boxes that can be present in a transaction (optional: default is %(default)s)')
+parser.add_argument('--minBox', '-bn', metavar='30', type=int, default=30, required=False, help='The minimum number of boxes that can be present in a transaction (optional: default is %(default)s)')
+parser.add_argument('--check', '-c', default=False, action='store_true', required=False, help='This parameter help to check information of tx before send it and question for send or no TX (optional: default is %(default)s)')
+
+args = parser.parse_args()
+
+
+ip = args.ip
+fee = int(args.fee * 1e9)
+max_value = int(args.maxValue * 1e9)
+api_key = args.apiKey
+oracle_address = args.oracleAddress
+merge_to_address = oracle_address if args.mergeToAddress == '' else args.mergeToAddress
+max_box = args.maxBox
+min_box = args.minBox
+check_param = args.check
+
+def log(text):
+	log_file = open("./merge_oracle_box.log", "a")
+	aDict = {"time": str(datetime.now()), "message": text}
+	print(text)
+	log_file.write(json.dumps(aDict) + "\n")
+	log_file.close()
+	
+
+try:
+	log("START APP:")
+	check(ip, api_key, fee, oracle_address, merge_to_address, max_value, max_box, min_box, check_param)
+except Exception as err:
+	log("Unexpected error:" + str(err))
